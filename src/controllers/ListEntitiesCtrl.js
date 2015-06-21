16c30262e29cd54a0a3a526545de121f5eb9059b
Luke Lanchester <luke@lukelanchester.com>
2015-06-21 13:00:06 +0100

    Changed format of results

--- src/controllers/ListEntitiesCtrl.js
+++ src/controllers/ListEntitiesCtrl.js
@@ -2,7 +2,9 @@ function ListEntitiesCtrl(req, res){
 
 	req.schema.getEntities(req.query)
 		.then(function(entities){
-			res.json(entities);
+			res.json({
+				results: entities
+			});
 		})
 		.catch(function(err){
 			res.status(500).json('DB error');
