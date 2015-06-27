24bcbdc8aa2d85abd58b5ef46c1bb82880dd3a2f
Luke Lanchester <luke@lukelanchester.com>
2015-06-27 21:43:47 +0100

    Update test example

--- test/fields.js
+++ test/fields.js
@@ -1,71 +1,16 @@
 var should = require('should');
 
 
-var VarcharField = require('../src/fields/VarcharField');
+var StringField = require('../src/classes/Field/StringField');
 
 
-describe('VarcharField', function(){
+describe('StringField', function(){
 
 
-	it('Can get key from field', function(){
-		var field = new VarcharField('name');
-		should.equal(field.key, 'name');
-	});
-
-
-	it('Will get default value', function(){
-		var field = new VarcharField('name', { default: 'hello' });
-		var result = field.getValueFromInput({}, null);
-		should.equal('hello', result.value);
-	});
-
-
-	it('Will get input value', function(){
-		var field = new VarcharField('name', { default: 'hello' });
-		var result = field.getValueFromInput({ name: 'world' }, null);
-		should.equal('world', result.value);
-	});
-
-
-	it('Will ignore input if protected', function(){
-		var field = new VarcharField('name', { protected: true, default: 'world' });
-		var result = field.getValueFromInput({ name: 'world' }, null);
-		should.equal('world', result.value);
-	});
-
-
-	it('Will ignore update if protected', function(){
-		var field = new VarcharField('name', { protected: true });
-		var result = field.getValueFromInput({ name: 'alpha' }, { name: 'bravo' });
-		should(true, result.skip);
-	});
-
-
-	it('Will use input if not editable', function(){
-		var field = new VarcharField('name', { editable: false });
-		var result = field.getValueFromInput({ name: 'world' }, null);
-		should.equal('world', result.value);
-	});
-
-
-	it('Will ignore update if not editable', function(){
-		var field = new VarcharField('name', { editable: false });
-		var result = field.getValueFromInput({ name: 'alpha' }, { name: 'bravo' });
-		should(true, result.skip);
-	});
-
-
-	it('Will error if required', function(){
-		var field = new VarcharField('name', { required: true });
-		var result = field.getValueFromInput({ }, null);
-		should.equal('required', result.error);
-	});
-
-
-	it('Will not error if required', function(){
-		var field = new VarcharField('name', { required: true });
-		var result = field.getValueFromInput({}, {});
-		should(true, result.skip);
+	it('Can get slug from field', function(){
+		var slug = 'test';
+		var field = new StringField({ slug: slug });
+		should.equal(field.slug, slug);
 	});
 
 
